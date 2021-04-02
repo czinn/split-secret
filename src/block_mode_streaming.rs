adc2bc7dd1e84d0e6a485588ea30e74edc553b0e
Charles Zinn <charles.zinn@gmail.com>
2021-04-02 12:53:25 -0400

    Slightly-more-working version of shamir_ida

--- src/block_mode_streaming.rs
+++ src/block_mode_streaming.rs
@@ -1,15 +1,19 @@
 use std::io::{Read, Write, Result};
+use std::ops::Range;
 use std::marker::{PhantomData};
 
-use block_modes::cipher::generic_array::GenericArray;
+use block_modes::cipher::generic_array::{GenericArray, ArrayLength};
 use block_modes::cipher::block::{BlockCipher, NewBlockCipher};
 use block_modes::{BlockMode, block_padding::Padding};
-use typenum::{Unsigned, U8, op};
+use typenum::Unsigned;
+use core::slice;
 
 pub enum Direction {
     Encrypt,
     Decrypt,
 }
+
+const BUF_SIZE: usize = 1024;
 
 pub struct ReadStream<'a, T, C, P>
 where T: BlockMode<C, P>,
@@ -17,52 +21,63 @@ where T: BlockMode<C, P>,
       P: Padding,
 {
     block_mode: T,
-    _c: PhantomData<C>,
-    _p: PhantomData<P>,
     reader: &'a mut dyn Read,
     direction: Direction,
-    foo: [u8; <C as BlockCipher>::BlockSize::ArrayType::USIZE],
-    //buf: GenericArray<u8, op!(<C as BlockCipher>::BlockSize * U8)>,
-    buf: [u8; <U8 as Unsigned>::USIZE],
-    filled_buf: &'a [u8],
+    buf: [u8; BUF_SIZE],
+    filled_buf: Range<usize>,
+    _c: PhantomData<C>,
+    _p: PhantomData<P>,
 }
 
-/*
+fn to_blocks<N>(data: &mut [u8]) -> &mut [GenericArray<u8, N>]
+where
+    N: ArrayLength<u8>,
+{
+    let n = N::to_usize();
+    debug_assert!(data.len() % n == 0);
+
+    #[allow(unsafe_code)]
+    unsafe {
+        slice::from_raw_parts_mut(data.as_ptr() as *mut GenericArray<u8, N>, data.len() / n)
+    }
+}
+
 impl<'a, T, C, P> ReadStream<'a, T, C, P>
 where T: BlockMode<C, P>,
       C: BlockCipher + NewBlockCipher,
       P: Padding,
 {
-    const BLOCK_SIZE: usize = <<C as BlockCipher>::BlockSize as Unsigned>::to_usize();
+    const BLOCK_SIZE: usize = <<C as BlockCipher>::BlockSize as Unsigned>::USIZE;
 
-    pub fn new(block_mode: T, reader: &'a mut dyn Read, direction: Direction) {
-        let buf = [0u8; Self::BLOCK_SIZE];
-        Self { block_mode, reader, direction, buf, filled_buf: &buf[0..0] }
+    pub fn new(block_mode: T, reader: &'a mut dyn Read, direction: Direction) -> Self {
+        let buf = [0u8; BUF_SIZE];
+        Self { block_mode, reader, direction, buf, filled_buf: 0..0, _c: PhantomData, _p: PhantomData }
     }
 
-    fn fill_buff(&mut self) -> Result<usize> {
+    fn fill_buf(&mut self) -> Result<usize> {
+        let usable_buf_size = BUF_SIZE - BUF_SIZE % Self::BLOCK_SIZE;
         if self.filled_buf.len() != 0 {
             return Ok(self.filled_buf.len());
         }
         let mut read_size = 0;
         loop {
-            match self.reader.read(self.buf[read_size..self.buf.len() - Self::BLOCK_SIZE])? {
+            match self.reader.read(&mut self.buf[read_size..usable_buf_size - Self::BLOCK_SIZE])? {
                 0 => break,
-                Ok(n) => read_size += n,
+                n => read_size += n,
             }
         }
         // Encrypt or decrypt the bytes in the buffer
-        match self.direction {
-            Encrypt => {
-                self.filled_buf = P::pad(&mut self.buf, read_size, Self::BLOCK_SIZE).unwrap();
-                let mut blocks = self.filled_buf.chunks(Self::BLOCK_SIZE).map(|block| GenericArray::from_slice(block)).collect();
+        match &self.direction {
+            Direction::Encrypt => {
+                self.filled_buf = 0..P::pad(&mut self.buf, read_size, Self::BLOCK_SIZE).unwrap().len();
+                let mut blocks = to_blocks(&mut self.buf[self.filled_buf.clone()]);
                 T::encrypt_blocks(&mut self.block_mode, &mut blocks);
             },
-            Decrypt => {
-                self.filled_buf = &self.buf[..read_size];
-                let mut blocks = self.filled_buf.chunks(Self::BLOCK_SIZE).map(|block| GenericArray::from_slice(block)).collect();
+            Direction::Decrypt => {
+                self.filled_buf = 0..read_size;
+                let mut blocks = to_blocks(&mut self.buf[self.filled_buf.clone()]);
                 T::decrypt_blocks(&mut self.block_mode, &mut blocks);
-                self.filled_buf = P::unpad(self.filled_buf).unwrap();
+                self.filled_buf = 0..P::unpad(&mut self.buf[self.filled_buf.clone()]).unwrap().len();
             },
         }
         Ok(self.filled_buf.len())
@@ -77,13 +92,68 @@ where T: BlockMode<C, P>,
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         let read_size = self.fill_buf()?;
         if buf.len() >= read_size {
-            buf[..read_size].copy_from_slice(self.filled_buf);
-            self.filled_buf = self.buf[0..0];
+            buf[..read_size].copy_from_slice(&self.buf[self.filled_buf.clone()]);
+            self.filled_buf = 0..0;
             Ok(read_size)
         } else {
-            buf.copy_from_slice(self.filled_buf[..buf.len()]);
-            self.filled_buf = self.filled_buf[buf.len()..];
+            let read_to = self.filled_buf.start + buf.len();
+            buf.copy_from_slice(&self.buf[self.filled_buf.start..read_to]);
+            self.filled_buf = read_to..self.filled_buf.end;
             Ok(buf.len())
         }
     }
-} */
+}
+
+pub struct WriteStream<'a, T, C, P>
+where T: BlockMode<C, P>,
+      C: BlockCipher + NewBlockCipher,
+      P: Padding,
+{
+    block_mode: T,
+    writer: &'a mut dyn Write,
+    direction: Direction,
+    buf: Vec<u8>,
+    buf_bytes: usize,
+    _c: PhantomData<C>,
+    _p: PhantomData<P>,
+}
+
+impl<'a, T, C, P> WriteStream<'a, T, C, P>
+where T: BlockMode<C, P>,
+      C: BlockCipher + NewBlockCipher,
+      P: Padding,
+{
+    const BLOCK_SIZE: usize = <<C as BlockCipher>::BlockSize as Unsigned>::USIZE;
+
+    pub fn new(block_mode: T, writer: &'a mut dyn Write, direction: Direction) -> Self {
+        Self { block_mode, writer, direction, buf: vec![u8; Self::BLOCK_SIZE], buf_bytes: 0, _c: PhantomData, _p: PhantomData }
+    }
+}
+
+impl<'a, T, C, P> Write for WriteStream<'a, T, C, P>
+where T: BlockMode<C, P>,
+      C: BlockCipher + NewBlockCipher,
+      P: Padding,
+{
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        let writeable_bytes = self.buf_bytes + buf.len();
+        if writeable_bytes < Self::BLOCK_SIZE {
+            // Copy bytes into the local buffer
+            self.buf[self.buf_bytes..self.buf_bytes + buf.len()].copy_from_slice(buf);
+            self.buf_bytes += buf.len();
+            return Ok(buf.len());
+        }
+        let bytes_to_copy_into_local_buf = Self::BLOCK_SIZE - self.buf_bytes;
+        self.buf[self.buf_bytes..self.buf.len()].copy_from_slice(buf[..bytes_to_copy_into_local_buf]);
+        // Process and write this single block
+        match &self.direction {
+            Direction::Encrypt => {
+                T::encrypt_blocks(&mut self.block_mode, &mut to_blocks(self.buf[..]));
+                self.writer.write_all(&self.buf[..])?;
+            }
+            Direction::Decrypt => {
+
+            }
+        }
+    }
+}
--- src/shamir_ida.rs
+++ src/shamir_ida.rs
@@ -1,17 +1,17 @@
 use std::io::{Read, Write, Cursor};
-use std::cmp;
 
 use crate::partitioner::{Partitioner, InputPartition, OutputPartition};
 use crate::shamir::Shamir;
 use crate::ida::Ida;
-use crate::bit_pad::{PaddedReader, PaddedWriter};
+use crate::block_mode_streaming::{ReadStream, Direction};
 
 use rand::rngs::OsRng;
 use rand::RngCore;
 use aes::Aes256;
-use aes::cipher::{BlockCipher, NewBlockCipher};
+use aes::cipher::NewBlockCipher;
 use aes::cipher::generic_array::GenericArray;
-use typenum::Unsigned;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Iso7816;
 
 pub struct ShamirIda {
     k: u8,
@@ -26,40 +26,21 @@ impl ShamirIda {
     }
 }
 
-const BUF_SIZE: usize = 1024;
-/*
 impl Partitioner for ShamirIda {
     fn split(&self, input: &mut impl Read, outputs: &mut Vec<OutputPartition>) {
-        let mut key = [0u8, 32];
+        let mut key = [0u8, 64];
         OsRng.fill_bytes(&mut key);
-        let cipher = Aes256::new(&GenericArray::from_slice(&key));
+        let cipher = Aes256::new(&GenericArray::from_slice(&key[..32]));
+        let block_mode: Cbc<Aes256, Iso7816> = Cbc::new(cipher, &GenericArray::from_slice(&key[32..]));
+        let mut input = ReadStream::new(block_mode, input, Direction::Encrypt);
 
         // Write the key using Shamir's secret sharing
         self.shamir.split(&mut Cursor::new(key), outputs);
 
-        let block_size = <<Aes256 as BlockCipher>::BlockSize as Unsigned>::to_usize();
-        let mut input = PaddedReader::new(block_size, input);
-        let target_read_size = BUF_SIZE - BUF_SIZE % block_size;
-
-        loop {
-            let mut read_size = 0;
-            loop {
-                match input.read(&mut read_buf[read_size..target_read_size]) {
-                    Err(_) | Ok(0) => break,
-                    Ok(block_read_size) =>
-                    {
-                        read_size += block_read_size;
-                    }
-                }
-            }
-            if read_size % k_usize != 0 {
-                panic!("input was not correctly padded");
-            }
-            if read_size == 0 {
-                break;
-            }
+        // Write the input using IDA
+        self.ida.split(&mut input, outputs);
     }
 
     fn join(&self, inputs: &mut Vec<InputPartition>, output: &mut impl Write) {
     }
-} */
+}
